0432a196730730e281d62358f43ff396ff7a4af1
chenze <chenzeggb@outlooke.com>
2026-07-07 14:05:05 +0800

    fix: 强行锚定导入标准库，防止 PyInstaller 动态遗漏

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 # main.py
 import sys
+
+# =========================================================================
+# [CRITICAL FOR PYINSTALLER]
+# Force PyInstaller's static analyzer to collect standard libraries
+# that are dynamically imported inside our sub-modules in the runtime.
+# =========================================================================
+import json
+import math
+import hashlib
+import random
+import datetime
+
 from PyQt6.QtWidgets import QApplication
 from ui.login import LoginDialog
 from ui.main_window import MainWindow
